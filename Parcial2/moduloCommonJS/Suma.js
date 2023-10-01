876a2c958eb6ff7af1003a465e7747fe83ab0dd5
EuniceDavila <davilaeunice3@gmail.com>
2023-10-01 14:16:36 -0500

    Modulo Suma

---
+++ Parcial2/moduloCommonJS/Suma.js
@@ -0,0 +1,20 @@
+/*function areaCuadrado(lado){
+    return lado*lado;
+    }
+    
+    function areaTriangulo(base,altura){
+    return ((base*altura)/2);
+    }
+    
+    module.exports.areaCuadrado=areaCuadrado;
+    module.exports.areaTriangulo=areaTriangulo;
+    
+    console.log(__dirname);
+    console.log(__filename);
+    console.log(module);*/
+    
+function suma(num1, num2) {
+    return num1 + num2;
+}
+
+module.exports.suma = suma;
