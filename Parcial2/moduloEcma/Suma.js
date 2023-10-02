c3193e78794839bbefa012fcdc607c8c6391bcdb
EuniceDavila <davilaeunice3@gmail.com>
2023-10-02 18:54:01 -0500

    Ejercicio moduloEcma

---
+++ Parcial2/moduloEcma/Suma.js
@@ -0,0 +1,18 @@
+/*function areaCuadrado(lado){
+    return lado*lado;
+    }
+    
+    function areaTriangulo(base,altura){
+    return ((base*altura)/2);
+    }
+    
+    module.exports.areaCuadrado=areaCuadrado;
+    module.exports.areaTriangulo=areaTriangulo;
+    
+    console.log(__dirname);
+    console.log(__filename);
+    console.log(module);*/
+    
+export function suma(num1, num2) {
+    return num1 + num2;
+}
